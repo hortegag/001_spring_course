c4a32e773438cce41d6f0f5cca1ad1daf17a399a
hortegag <hog_andy@hotmail.com>
2018-03-05 21:33:29 -0500

    015 field injection Autowired and Qualifier

---
+++ 002_spring_hibernate/src/com/home/springdemo/RandomFortuneService.java
@@ -0,0 +1,28 @@
+package com.home.springdemo;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomFortuneService implements FortuneService {
+	
+	private String[] data ={
+		"Beware fo the wolf in sheep's clothing",
+		"Diligence is the mother of good luck",
+		"The journey is the reward"
+	};
+	
+	private Random random = new Random();
+
+	@Override
+	public String getFortune() {
+		// TODO Auto-generated method stub
+		
+		int index = random.nextInt(data.length);
+		String fortune = data[index];
+		
+		return fortune;
+	}
+
+}
--- 002_spring_hibernate/src/com/home/springdemo/TennisCoach.java
+++ 002_spring_hibernate/src/com/home/springdemo/TennisCoach.java
@@ -1,6 +1,7 @@
 package com.home.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 public class TennisCoach implements Coach {
     
 	@Autowired
+	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
 
 	  
