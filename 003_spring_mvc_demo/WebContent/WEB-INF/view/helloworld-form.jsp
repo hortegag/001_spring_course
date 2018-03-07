ca2b9573e9f265d5472ad0a1869a0ef9d7679dc1
hortegag <hog_andy@hotmail.com>
2018-03-06 22:48:16 -0500

    023 Reading data from the tag request param

--- 003_spring_mvc_demo/WebContent/WEB-INF/view/helloworld-form.jsp
+++ 003_spring_mvc_demo/WebContent/WEB-INF/view/helloworld-form.jsp
@@ -5,7 +5,7 @@
 	<title>Hello World - Input Form</title>
 </head>
 	<body>
-		<form action="processFormVersionTwo" mehod="GET">
+		<form action="processFormVersionThree" mehod="GET">
 			<input type="text" name="studentName" placeholder="Wat's your name?"/>
 			<input type="submit"/>
 		</form>
--- 003_spring_mvc_demo/src/com/home/springdemo/mvc/HelloWorldController.java
+++ 003_spring_mvc_demo/src/com/home/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -47,5 +48,27 @@ public class HelloWorldController {
 		
 		return "helloworld";
 	}
+	
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName") String studentName, Model model){
+		
+		
+		// read the request parameter from the HTML form
+		String name = studentName;
+		
+		
+		// convert the data to all caps
+		name = name.toUpperCase();
+		
+		// create the message
+		String result = "Hey mY Frined from v3!: " + name;
+		
+		// add message to the model
+		model.addAttribute("message",result);
+		
+		return "helloworld";
+	}
+	
 	
 }
