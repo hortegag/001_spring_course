2b09916ba7974fa3f3eef43c0f5892a83b5f7a9c
hortegag <hog_andy@hotmail.com>
2018-03-20 00:20:07 -0500

    056 one to many uniderectional delete course and reviews

---
+++ 008_hibernate_one_to_uni/src/com/home/demo/DeleteCoursesAndReviewsDemo.java
@@ -0,0 +1,61 @@
+package com.home.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.home.entity.Course;
+import com.home.entity.Instructor;
+import com.home.entity.InstructorDetail;
+import com.home.entity.Review;
+
+public class DeleteCoursesAndReviewsDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								 .configure("hibernate.cfg.xml")
+								 .addAnnotatedClass(Instructor.class)
+								 .addAnnotatedClass(InstructorDetail.class)
+								 .addAnnotatedClass(Course.class)
+								 .addAnnotatedClass(Review.class)
+								 .buildSessionFactory();
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		// 
+		try {
+
+			// start a transaction
+			session.beginTransaction();
+			
+			
+			// get the course
+			int id = 10;
+			Course course = session.get(Course.class, id);
+			
+			// print the course
+			System.out.println("Course: "+course);
+			
+			// print the reviews
+			System.out.println(course.getReviews());
+		
+			session.delete(course);
+			
+			// commmit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			session.close();
+			factory.close();
+		}
+		
+
+	}
+
+}
