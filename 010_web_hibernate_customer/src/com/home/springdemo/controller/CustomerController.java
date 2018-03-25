e5ca9c097ca8940d8f7e35dfa4c035b922f2b7d5
hortegag <hog_andy@hotmail.com>
2018-03-25 17:59:03 -0500

    066 refactoring with getmapping and postmapping

--- 010_web_hibernate_customer/src/com/home/springdemo/controller/CustomerController.java
+++ 010_web_hibernate_customer/src/com/home/springdemo/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.home.springdemo.dao.CustomerDAO;
@@ -21,7 +22,11 @@ public class CustomerController {
 	
 	
 	
-	@RequestMapping("list")
+	//@RequestMapping("list")
+	// GetMapping allow to accept only get request
+	// PostMapping allow to accept only post request
+	
+	@GetMapping("/list")
 	public String listCustomer(Model model){
 		
 		// get customers from the dao
