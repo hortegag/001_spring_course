680198f65831f77919be7b189fb01f4de5f44f91
hortegag <hog_andy@hotmail.com>
2018-03-28 11:38:02 -0500

    085 aop after throwing an exception

---
+++ 016_spring_aop_after_throwing/src/com/home/aopdemo/MainDemoApp.java
@@ -0,0 +1,63 @@
+package com.home.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.home.aopdemo.dao.AccountDAO;
+import com.home.aopdemo.dao.MembershipDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		
+		
+		// read spring config java class
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		
+		
+		
+		// get the bean from spring container
+		AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+	
+		// call the business mehtod
+		accountDAO.addAccount();
+		
+		System.out.println("Second call");
+		accountDAO.addAccount();
+		
+		
+		System.out.println("call addtest");
+		accountDAO.addTestAccount();
+		
+		
+		MembershipDAO membershipDAO = context.getBean("membershipDAO",MembershipDAO.class);
+		
+		membershipDAO.addAccount();
+				
+		System.out.println("add test member accout");
+		membershipDAO.addTesMemberAccount();
+		
+		Account account = new Account();
+		account.setLevel("top level");
+		account.setName("Hecthor ");
+		
+		System.out.println("adding a new accoutn with a parameter");
+		accountDAO.addNewAccount(account);
+		
+		
+		System.out.println("adding a important person account");
+		accountDAO.addNewAccount(account,true);
+		
+		System.out.println("\n\nGet methods");
+		accountDAO.getExample();
+		System.out.println("\n\nSet Methods");
+		accountDAO.setExample();
+		
+		// close the context
+		context.close();
+
+	}
+
+}
