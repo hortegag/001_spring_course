28fec300f570cbb26934c1b9a2ae72a21dcfc2fb
hortegag <hog_andy@hotmail.com>
2018-03-29 09:52:28 -0500

    092 creating standalone and webapp with maven

---
+++ 022_maven_quick_start/firstmavenproject/src/main/java/com/home/firstmavenproject/App.java
@@ -0,0 +1,13 @@
+package com.home.firstmavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Héctor Ortega" ); 
+    }
+}
